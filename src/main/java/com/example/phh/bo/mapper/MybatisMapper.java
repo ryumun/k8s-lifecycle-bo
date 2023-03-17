eb11e54b2cfc83a8591136346d5ec825bc62937b
MZC01-RYUMUN <MZC01-RYUMUN@MZC01-RYUMUN>
2023-03-17 13:31:48 +0900

    .

---
+++ src/main/java/com/example/phh/bo/mapper/MybatisMapper.java
@@ -0,0 +1,17 @@
+package com.example.phh.bo.mapper;
+
+import java.util.List;
+
+import com.example.phh.bo.model.Todo;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface MybatisMapper {
+    public List<Todo> findAll();
+    public Todo findById(String id);
+    public int add(Todo model);
+    public int addTest();
+    public int modify(Todo model);
+    public int remove(String id);
+}
